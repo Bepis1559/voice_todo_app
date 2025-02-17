685e130ab3baef2045d4654cf03643e56d31f4fb
Bepis1559 <axl2001@abv.bg>
2025-02-17 20:31:23 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <main>
       <WeeklyTasksTable weeklyTasks={weeklyTasks} />
-      <br />
       {transcript}
       <SpeechButtonsList speak={speak} baseSpeakOptions={baseSpeakOptions} />
     </main>
