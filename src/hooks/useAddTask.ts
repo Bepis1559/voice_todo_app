6f429f2cc15c87fcc83781fb2525279aafa5c331
Bepis1559 <axl2001@abv.bg>
2025-02-17 21:16:00 +0200

    switched state with local storage

--- src/hooks/useAddTask.ts
+++ src/hooks/useAddTask.ts
@@ -1,18 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSpeechRecognition } from "react-speech-recognition";
 import { useSpeechSynthesis } from "react-speech-kit";
 import { useCommands } from "./useCommands";
 import { getBaseSpeakOptions } from "../helpers/getBaseSpeakOptions";
+import { useLocalStorage } from "usehooks-ts";
 export function useAddTask() {
-  const [weeklyTasks, setWeeklyTasks] = useState<weeklyTasks>({
-    Monday: [],
-    Tuesday: [],
-    Wednesday: [],
-    Thursday: [],
-    Friday: [],
-    Saturday: [],
-    Sunday: [],
-  });
+  const [weeklyTasks, setWeeklyTasks] = useLocalStorage<weeklyTasks>(
+    "weeklyTasks",
+    {
+      Monday: [],
+      Tuesday: [],
+      Wednesday: [],
+      Thursday: [],
+      Friday: [],
+      Saturday: [],
+      Sunday: [],
+    },
+  );
   const { speak, voices, cancel } = useSpeechSynthesis();
   const baseSpeakOptions = getBaseSpeakOptions(voices);
 
